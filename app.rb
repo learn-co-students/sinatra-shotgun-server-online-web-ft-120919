45a6256bdc89b3be1761ca05f4424f658bfa050b
github username <github email address>
2020-02-03 15:47:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to my first web app!!!! Shotgun is awesome"
   end
 
 end
